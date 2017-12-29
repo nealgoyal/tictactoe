1d98c2491f3245acfdb77eecbd613581e0562f96
Neal Goyal <ngoya001@ucr.edu>
2017-12-29 15:55:15 -0800

    Functionality for winning move

--- game.py
+++ game.py
@@ -11,16 +11,32 @@ def display_board(board):
 
 # Creating player input
 def player_input():
-    pos = input("Enter position: ")
-    while pos not in board:
-        pos = input("Enter position: ")
+    marker = ''
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
 
-    return pos
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
 
 # Implementing 'X' and 'O' on board from user player_input
 def place_marker(board, marker, position):
     board[position] = marker
 
+# Checking if there is a winner!
+def win_check(board,mark):
+            # horizontal
+    return  ((board[7] == mark and board[8] == mark and board[9] == mark) 
+            or (board[4] == mark and board[5] == mark and board[6] == mark)
+            or (board[1] == mark and board[2] == mark and board[3] == mark)
+            # vertical
+            or (board[7] == mark and board[4] == mark and board[1] == mark)
+            or (board[8] == mark and board[5] == mark and board[2] == mark)
+            or (board[9] == mark and board[6] == mark and board[3] == mark)
+            # diagonal
+            or (board[7] == mark and board[5] == mark and board[3] == mark)
+            or (board[9] == mark and board[5] == mark and board[1] == mark))
 
 board = range(1,10)
 print (display_board(board))
